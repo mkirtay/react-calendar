041381c64524e7aa63b31ab1a064dbc71d973526
TURKCELL\ext02d11182 <mucahid.kirtay@consultant.turkcell.com.tr>
2019-03-18 14:45:31 +0300

    next ve prev buton fonksiyonlari calisir hale geldi

--- src/Components/CalendarDate.js
+++ src/Components/CalendarDate.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
-export default class CalendarDate extends Component{
+export default class CalendarDate extends Component {
 
-    state = { totalDays: [], month: new Date().getMonth(), year: new Date().getFullYear() };
+    state = {totalDays: [], month: new Date().getMonth(), year: new Date().getFullYear()};
 
-    showCalendar(month, year){
-        const firstDay =  new Date(year, month).getDay();
+    showCalendar(month, year) {
+        const firstDay = new Date(year, month).getDay();
         let totalDays = [];
 
-        for( let i = 1; i < firstDay; i++ ){
-            totalDays.push( '' )
+        for (let i = 1; i < firstDay; i++) {
+            totalDays.push('')
         }
-        for( let i = 1; i < this.props.daysInMonth(month, year) + 1; i++ ){
-            totalDays.push( i )
+        for (let i = 1; i < this.props.daysInMonth(month, year) + 1; i++) {
+            totalDays.push(i)
         }
 
         this.setState(
-            { totalDays: totalDays }
+            {totalDays: totalDays}
         )
     }
 
-    componentDidMount(){
+    componentDidMount() {
         /*const today = new Date();
         const currentMonth = today.getMonth();
         const currentYear = today.getFullYear();
@@ -33,27 +33,17 @@ export default class CalendarDate extends Component{
         this.showCalendar(this.state.month, this.state.year);
     }
 
-    onClickPrev(){
-        let month = this.state.month;
-        let year = this.state.year;
-
-        ( month === 0 ) ? this.setState({ year: year-1}) : this.setState({ year: year });
-        ( month === 0 ) ? this.setState({ month: 11 }): this.setState({ month: month-1 });
-
-        this.showCalendar(this.state.month, this.state.year);
-    }
-
-    onChangeMonth(e){
+    onChangeMonth(e) {
         const selectValueMonth = e.target.value;
 
         this.setState({
-           month: selectValueMonth
+            month: selectValueMonth
         });
 
         this.showCalendar(selectValueMonth, this.state.year);
     }
 
-    onChangeYear(e){
+    onChangeYear(e) {
         const selectValueYear = e.target.value;
 
         this.setState({
@@ -63,48 +53,62 @@ export default class CalendarDate extends Component{
         this.showCalendar(this.state.month, selectValueYear);
     }
 
-    onClickNext(){
+
+    onClickPrev() {
         let month = this.state.month;
         let year = this.state.year;
 
-        ( month === 11 ) ? this.setState({ year: year+1 }) : this.setState({ year: year });
-        this.setState({ month: (month+1) % 12 });
+        year = ( month === 0 ) ? year - 1: year;
+        month = ( month === 0 ) ? 11 : (month-1);
 
-        this.showCalendar(this.state.month, this.state.year);
+        this.showCalendar(month, year);
+        this.setState({ month: month, year: year })
     }
 
-    render(){
-        const dayName = this.props.days.map( (days, i) => {
-            return(
+
+    onClickNext() {
+        let month = this.state.month;
+        let year = this.state.year;
+
+        year = ( month === 11 ) ? year + 1 : year;
+        month = (month + 1) % 12;
+
+        this.showCalendar(month, year);
+        this.setState({month:month,year:year});
+    }
+
+    render() {
+        const dayName = this.props.days.map((days, i) => {
+            return (
                 <span key={i} id={i}>{days.day}</span>
             )
         });
 
-        const monthName = this.props.months.map( (months, i) => {
-            return(
+        const monthName = this.props.months.map((months, i) => {
+            return (
                 <option key={i} name={months.month} value={i}>{months.month}</option>
             )
         });
 
-        const year = this.props.years.map( (years, i) => {
-            return(
-                <option key={i} value={i}> {years.year} </option>
+        const year = this.props.years.map((years, i) => {
+            return (
+                <option key={i} value={years.year}> {years.year} </option>
             )
         });
 
         return (
             <div>
-                <h3 className="calendar__title "> { this.props.months[this.state.month].month }  { this.state.year }  </h3>
+                <h3 className="calendar__title "> {this.props.months[this.state.month].month} {this.state.year}  </h3>
                 <div className="calendar-date">
                     <div className="calendar-date__head">
                         {dayName}
                     </div>
                     <div className="calendar-date__body">
-                        {this.state.totalDays.map( (day, i) => {
-                            return(
+                        {this.state.totalDays.map((day, i) => {
+                            return (
                                 <span className="sss" key={i}> {day} </span>
                             )
-                        } )}
+                        })}
                     </div>
                 </div>
                 <div className="buttons">
